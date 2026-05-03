b173b5a76f0d204b679c93feefbb52f849bf25ab
mohamedsaad21 <mohamedsaad2756@gmail.com>
2026-05-03 17:25:27 +0300

    Edit GetUserByIdResponse

--- Fixy/Fixy.Application/Features/Users/Queries/GetUserById/GetUserByIdQueryHandler.cs
+++ Fixy/Fixy.Application/Features/Users/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -1,24 +1,46 @@
 ﻿using AutoMapper;
 using Fixy.Application.Bases;
+using Fixy.Application.Common.Helpers;
+using Fixy.Application.Resources;
 using Fixy.Domain.Entities;
 using Fixy.Domain.Entities.Identity;
 using Fixy.Domain.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
 
 namespace Fixy.Application.Features.Users.Queries.GetUserById;
 
-public sealed class GetUserByIdQueryHandler(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, IMapper mapper) : IRequestHandler<GetUserByIdQuery, Result<GetUserByIdResponse>>
+public sealed class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, Result<GetUserByIdResponse>>
 {
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IMapper _mapper;
+    private readonly IStringLocalizer<SharedResources> _localizer;
+
+    public GetUserByIdQueryHandler(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager,
+        IMapper mapper, IStringLocalizer<SharedResources> localizer)
+    {
+        _unitOfWork = unitOfWork;
+        _userManager = userManager;
+        _mapper = mapper;
+        _localizer = localizer;
+    }
+
     public async Task<Result<GetUserByIdResponse>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
     {
-        var user = await userManager.FindByIdAsync(request.Id.ToString());
+        var user = await _userManager.FindByIdAsync(request.Id.ToString());
         if (user == null)
             return Errors.UserNotFound;
 
-        var response = mapper.Map<GetUserByIdResponse>(user);
-
-        var roles = await userManager.GetRolesAsync(user);
+        var response = _mapper.Map<GetUserByIdResponse>(user);
+        response.Status = user switch
+        {
+            Customer customer => EnumLocalizer.Localize(customer.Status, _localizer),
+            Technician technician => EnumLocalizer.Localize(technician.Status, _localizer),
+            _ => _localizer[SharedResourcesKeys.AdminActiveStatus]
+        };
+        var roles = await _userManager.GetRolesAsync(user);
         response.Role = roles.FirstOrDefault()!;
         return response;
     }
--- Fixy/Fixy.Application/Features/Users/Queries/GetUserById/GetUserByIdResponse.cs
+++ Fixy/Fixy.Application/Features/Users/Queries/GetUserById/GetUserByIdResponse.cs
@@ -9,5 +9,6 @@ public class GetUserByIdResponse
     public string Email { get; set; }
     public string ProfilePictureUrl { get; set; }
     public string Role { get; set; }
+    public string Status { get; set; }
     public bool IsTwoFactorEmailEnabled { get; set; }
 }
