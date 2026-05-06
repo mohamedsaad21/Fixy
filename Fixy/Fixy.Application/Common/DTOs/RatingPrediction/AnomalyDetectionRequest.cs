745cc9b8d24ff8f1c84b91595d58bbbdc1efa790
mohamedsaad21 <mohamedsaad2756@gmail.com>
2026-05-07 00:21:27 +0300

    Finish Integration With Anomaly Detection Model

---
+++ Fixy/Fixy.Application/Common/DTOs/RatingPrediction/AnomalyDetectionRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Fixy.Application.Common.DTOs.RatingPrediction;
+
+public class AnomalyDetectionRequest
+{
+    public double UserAvgRating { get; set; }
+    public double WorkerAvgRating { get; set; }
+    public double UserRatingStd { get; set; }
+    public double UserWorkerAvg { get; set; }
+    public int FeedbackCount { get; set; }
+    public double WorkerRatingVar { get; set; }
+    public double RatingDeviation { get; set; }
+    public double predictedRating { get; set; }
+}
---
+++ Fixy/Fixy.Application/Common/DTOs/RatingPrediction/AnomalyDetectionResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fixy.Application.Common.DTOs.RatingPrediction;
+
+public class AnomalyDetectionResponse
+{
+    [JsonPropertyName("predicted_anomaly")]
+    public int PredictedAnomaly { get; set; }
+    [JsonPropertyName("error")]
+    public string? Error { get; set; }
+}
