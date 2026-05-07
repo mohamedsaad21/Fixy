1a0562d6448c65fcbb187a166a2d9ac09eb5f110
Mohamed Saad <mohamedsaad2756@gmail.com>
2026-05-08 02:45:56 +0300

    Edit GetUserProfileById and GetUserInfo For Admin Responses (#42)

--- Fixy/Fixy.Application/Features/Admin/Queries/GetUserInfoById/GetUserInfoByIdQueryHandler.cs
+++ Fixy/Fixy.Application/Features/Admin/Queries/GetUserInfoById/GetUserInfoByIdQueryHandler.cs
@@ -1,13 +1,19 @@
 ﻿using AutoMapper;
 using Fixy.Application.Bases;
+using Fixy.Application.Common.Helpers;
+using Fixy.Application.Resources;
 using Fixy.Domain.Entities;
 using Fixy.Domain.Entities.Identity;
+using Fixy.Domain.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 
 namespace Fixy.Application.Features.Admin.Queries.GetUserInfoById;
 
-public sealed class GetUserInfoByIdQueryHandler(UserManager<ApplicationUser> userManager, IMapper mapper) : IRequestHandler<GetUserInfoByIdQuery, Result<GetUserInfoByIdResponse>>
+public sealed class GetUserInfoByIdQueryHandler(UserManager<ApplicationUser> userManager, IUnitOfWork unitOfWork,
+    IMapper mapper, IStringLocalizer<SharedResources> localizer) : IRequestHandler<GetUserInfoByIdQuery, Result<GetUserInfoByIdResponse>>
 {
     public async Task<Result<GetUserInfoByIdResponse>> Handle(GetUserInfoByIdQuery request, CancellationToken cancellationToken)
     {
@@ -24,10 +30,20 @@ public sealed class GetUserInfoByIdQueryHandler(UserManager<ApplicationUser> use
             result.NationalId = technician.NationalId;
             result.NationalIdCardImageUrl = technician.NationalIdCardImageUrl;
             result.AverageRating = technician.AverageRating;
+            result.Status = EnumLocalizer.Localize(technician.Status, localizer);
+            // Get Service Category Of Technician
+            var serviceCategory = await unitOfWork.ServiceCategories.GetTableNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == technician.ServiceCategoryId);
+
+            if (serviceCategory == null)
+                return Errors.ServiceCategoryNotFound;
+
+            result.ServiceCategory = serviceCategory.Localize(serviceCategory.NameAr, serviceCategory.NameEn);
 
         } else if(user is Customer customer)
         {
             result.NationalId = customer.NationalId;
+            result.Status = EnumLocalizer.Localize(customer.Status, localizer);
         }
         return result;
     }
