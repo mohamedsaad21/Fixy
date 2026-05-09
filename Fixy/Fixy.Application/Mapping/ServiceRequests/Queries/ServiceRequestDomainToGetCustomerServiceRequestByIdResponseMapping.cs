8a589edf40cb391de216017f7a46d37399e7465c
Mohamed Saad <mohamedsaad2756@gmail.com>
2026-05-09 22:39:58 +0300

    Handle user profile settings (#50)
    * Resolve Merge Conflicts
    
    * Finish Azure Deployment
    
    * Edit configurations
    
    * Handle GetCustomerServiceRequestById

--- Fixy/Fixy.Application/Mapping/ServiceRequests/Queries/ServiceRequestDomainToGetCustomerServiceRequestByIdResponseMapping.cs
+++ Fixy/Fixy.Application/Mapping/ServiceRequests/Queries/ServiceRequestDomainToGetCustomerServiceRequestByIdResponseMapping.cs
@@ -15,6 +15,7 @@ public partial class ServiceRequestProfile : Profile
             .ForMember(dest => dest.ServiceCategories, opt => opt.MapFrom(src => src.ServiceCategories.Select(x => x.Localize(x.NameAr, x.NameEn)).ToList()))
             .ForMember(dest => dest.Address, opt => opt.MapFrom(src => new AddressDto(src.Address.Country, src.Address.City, src.Address.Area, src.Address.Street, src.Address.BuildingNumber, src.Address.Latitude, src.Address.Longitude)))
             .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.ServiceRequestImages))
+            .ForMember(dest => dest.PriceOffers, opt => opt.MapFrom(src => src.PriceOffers))
             .ForMember(dest => dest.ScheduledDateTime, opt => opt.MapFrom(src => src.ScheduledDateTime.ToEgyptTime()));
 
     }
